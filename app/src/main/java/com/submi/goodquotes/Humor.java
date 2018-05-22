3e15270d182bfabe4208f1a98e0ed6a92aef8e91
madHEYsia <submiitr07@gmail.com>
2018-05-23 01:45:08 +0530

    feature - quotes reference added and share option added in all quotes

--- app/src/main/java/com/submi/goodquotes/Humor.java
+++ app/src/main/java/com/submi/goodquotes/Humor.java
@@ -1,12 +1,21 @@
 package com.submi.goodquotes;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Humor extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -44,7 +53,66 @@ public class Humor extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_humor, container, false);
+        View mContainer = inflater.inflate(R.layout.fragment_humor, null);
+        LinearLayout linearLayout = mContainer.findViewById(R.id.humour_layout);
+        quoteArray ob = new quoteArray();
+        final String array[][] = ob.quotes;
+        for (int i=224;i<314;++i){
+            TextView quote = new TextView(getActivity());
+            quote.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.FILL_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            quote.setPadding(40,40,40,0);
+            quote.setText(array[i][0]);
+            quote.setTypeface(null, Typeface.BOLD);
+            quote.setTextSize(TypedValue.COMPLEX_UNIT_SP, 19);
+            final int finalI = i;
+            quote.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent shareIntent = new Intent();
+                    shareIntent.setAction(Intent.ACTION_SEND);
+                    shareIntent.putExtra(Intent.EXTRA_TEXT,array[finalI][0]+" "+array[finalI][1]);
+                    shareIntent.setType("text/plain");
+                    startActivity(shareIntent);
+                }
+            });
+
+            TextView writer = new TextView(getActivity());
+            writer.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.FILL_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            writer.setPadding(40,0,40,40);
+            writer.setText(array[i][1]);
+            writer.setGravity(Gravity.END);
+            writer.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+            writer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent shareIntent = new Intent();
+                    shareIntent.setAction(Intent.ACTION_SEND);
+                    shareIntent.putExtra(Intent.EXTRA_TEXT,array[finalI][0]+" "+array[finalI][1]);
+                    shareIntent.setType("text/plain");
+                    startActivity(shareIntent);
+                }
+            });
+
+            View separator = new View(getActivity());
+            separator.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    10));
+            separator.setBackgroundColor(Color.parseColor("#B3B3B3"));
+
+            try{
+                linearLayout.addView(quote);
+                linearLayout.addView(writer);
+                linearLayout.addView(separator);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        return mContainer;
     }
 
     public void onButtonPressed(Uri uri) {
--- app/src/main/java/com/submi/goodquotes/Success.java
+++ app/src/main/java/com/submi/goodquotes/Success.java
@@ -1,12 +1,19 @@
 package com.submi.goodquotes;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Success extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -44,7 +51,67 @@ public class Success extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_success, container, false);
+        View mContainer = inflater.inflate(R.layout.fragment_success, null);
+        LinearLayout linearLayout = mContainer.findViewById(R.id.success_layout);
+        quoteArray ob = new quoteArray();
+        final String array[][] = ob.quotes;
+        for (int i=557;i<670;++i){
+            TextView quote = new TextView(getActivity());
+            quote.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.FILL_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            quote.setPadding(40,40,40,0);
+            quote.setText(array[i][0]);
+            quote.setTypeface(null, Typeface.BOLD);
+            quote.setTextSize(TypedValue.COMPLEX_UNIT_SP, 19);
+            final int finalI = i;
+            quote.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent shareIntent = new Intent();
+                    shareIntent.setAction(Intent.ACTION_SEND);
+                    shareIntent.putExtra(Intent.EXTRA_TEXT,array[finalI][0]+" "+array[finalI][1]);
+                    shareIntent.setType("text/plain");
+                    startActivity(shareIntent);
+                }
+            });
+
+            TextView writer = new TextView(getActivity());
+            writer.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.FILL_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            writer.setPadding(40,0,40,40);
+            writer.setText(array[i][1]);
+            writer.setGravity(Gravity.END);
+            writer.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+            writer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent shareIntent = new Intent();
+                    shareIntent.setAction(Intent.ACTION_SEND);
+                    shareIntent.putExtra(Intent.EXTRA_TEXT,array[finalI][0]+" "+array[finalI][1]);
+                    shareIntent.setType("text/plain");
+                    startActivity(shareIntent);
+                }
+            });
+
+            View separator = new View(getActivity());
+            separator.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    10));
+            separator.setBackgroundColor(Color.parseColor("#B3B3B3"));
+
+            try{
+                linearLayout.addView(quote);
+                linearLayout.addView(writer);
+                linearLayout.addView(separator);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        return mContainer;
+
     }
 
     public void onButtonPressed(Uri uri) {
